7665f958e6a57318fd17583d998ebc7228f068fb
Renan Cavalcanti <renan_cavalcanti@hotmail.com>
2026-03-19 15:20:35 -0400

    chore: register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,22 +1,54 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
+const register = async (req, res) => {
+    try{
+        const { name, email, password } = req.body;
 
-const register = (req, res) => {
+        console.log(name, email, password);
 
-    console.log(req.body);
-    const { name, email, password } = req.body;
+        if(!name || !email || !password){
+            return res.status(400).json({
+                message: "Name, Email and Password are required!"
+            });
+        }
 
-    console.log(name, email, password);
+        const existingUser = await User.findOne({email: String(email).toLowerCase()});
 
-    if(!name || !email || !password){
-        return res.status(400).json({
-            message: "Name, Email and Password are required!"
+        if(existingUser){
+            return res.status(409).json({
+                message: "Email is already registered!"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = await User.create({
+            name,
+            email,
+            password: hashedPassword
+        });
+
+        return res.status(201).json({
+            message: "User registered successfully.",
+            data: {
+                user:{
+                    id: user._id,
+                    name: user.name,
+                    email: user.email,
+                    createdAt: user.createdAt,
+                    updatedAt: user.updatedAt
+                }
+            }
         });
     }
-
-
-    return res.json({
-        message: "Register endpoint is working!"
-    });
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            message: "Error while registering user."
+        });
+    }
+    
 };
 
 
